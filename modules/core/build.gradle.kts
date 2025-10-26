481c2aee88a900dc9e04d3d6ec31399de8bab559
Zymus <moore.zyle@gmail.com>
2025-10-25 17:30:51 -0700

    Add publishing properties

--- modules/core/build.gradle.kts
+++ modules/core/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    id("maven-publish")
 }
+
+group = "games.studiohummingbird.skhema"
+version = "0.1.0-v29.3"
 
 repositories {
     gradlePluginPortal()
