4a53da20262812ecba4e41f5ac5befb8fabeb369
Zymus <moore.zyle@gmail.com>
2025-11-05 20:10:47 -0800

    Remove Intangible factory function

--- modules/core/src/commonMain/kotlin/games/studiohummingbird/skhema/types/Intangible.kt
+++ modules/core/src/commonMain/kotlin/games/studiohummingbird/skhema/types/Intangible.kt
@@ -16,24 +16,5 @@
  */
 package games.studiohummingbird.skhema.types
 
-import games.studiohummingbird.skhema.properties.*
-import games.studiohummingbird.skhema.types.mutable.MutableIntangible
-
 interface Intangible
     : Thing
-
-fun Intangible(block: MutableIntangible.() -> Unit): Intangible =
-    object : MutableIntangible {
-        override var additionalType: AdditionalType? = null
-        override var alternateName: AlternateName? = null
-        override var description: Description? = null
-        override var disambiguationDescription: DisambiguationDescription? = null
-        override var identifier: Identifier? = null
-        override var image: Image? = null
-        override var mainEntityOfPage: MainEntityOfPage? = null
-        override var name: Name? = null
-        override var potentialAction: PotentialAction? = null
-        override var sameAs: SameAs? = null
-        override var subjectOf: SubjectOf? = null
-        override var url: URL? = null
-    }.apply(block)
