39a1883ce11af5839ad5a7df507c95caabf61055
Zymus <moore.zyle@gmail.com>
2025-11-08 13:40:31 -0800

    Add Property modules for Text

---
+++ modules/serializable/src/commonMain/kotlin/games/studiohummingbird/skhema/serializable/modules/properties/PropertyIDModule.kt
@@ -0,0 +1,28 @@
+/* PropertyIDModule.kt
+ * Copyright (C) 2025  Zymus
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package games.studiohummingbird.skhema.serializable.modules.properties
+
+import games.studiohummingbird.skhema.properties.PropertyID
+import games.studiohummingbird.skhema.serializable.datatypes.serializableText
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+val PropertyIDModule = SerializersModule {
+    polymorphic(PropertyID::class) {
+        serializableText()
+    }
+}
