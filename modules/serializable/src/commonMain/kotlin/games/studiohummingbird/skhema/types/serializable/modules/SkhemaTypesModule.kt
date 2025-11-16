7b5d2a2af19cdf9d4692966d3066fc089903d4ff
Zymus <moore.zyle@gmail.com>
2025-11-15 21:22:13 -0800

    Sort types module again

--- modules/serializable/src/commonMain/kotlin/games/studiohummingbird/skhema/types/serializable/modules/SkhemaTypesModule.kt
+++ modules/serializable/src/commonMain/kotlin/games/studiohummingbird/skhema/types/serializable/modules/SkhemaTypesModule.kt
@@ -157,6 +157,6 @@ val SkhemaTypesModule = SerializersModule {
     include(WantActionModule)
     include(WatchActionModule)
     include(WearActionModule)
-    include(WriteActionModule)
     include(WinActionModule)
+    include(WriteActionModule)
 }
